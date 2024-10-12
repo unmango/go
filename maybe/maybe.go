ea2f294d3c5a704b5266753fbc4ab6a87d4a0c34
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-12 11:14:17 -0500

    yikes

--- maybe/maybe.go
+++ maybe/maybe.go
@@ -2,12 +2,15 @@ package maybe
 
 type Maybe[T any] func() (T, error)
 
-type none struct{}
+type e struct{}
 
 // Error implements error.
-func (n none) Error() string { return "None" }
+func (e) Error() string { return "None" }
 
-var ErrNone error = none{}
+var (
+	ErrNone error = e{}
+	ptrs    map[interface{}]interface{}
+)
 
 func Ok[T any](v T) Maybe[T] {
 	return func() (T, error) {
@@ -15,8 +18,8 @@ func Ok[T any](v T) Maybe[T] {
 	}
 }
 
-func None() (any, error) {
-	return nil, ErrNone
+func None[T any]() (T, error) {
+	return zero[T](), ErrNone
 }
 
 func IsOk[T any](m Maybe[T]) bool {
@@ -24,11 +27,16 @@ func IsOk[T any](m Maybe[T]) bool {
 }
 
 func IsNone[T any](m Maybe[T]) bool {
-	_, n := m()
-	return n == ErrNone
+	n := None[T]
+	if p, ok := ptrs[&n]; ok {
+		return true
+	} else {
+		
+	}
 }
 
 func zero[T any]() T {
+	// TODO: Something something allocations
 	var t T
 	return t
 }
