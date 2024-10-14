2081d0a3903215fa1ae3ef5ae12745a05c551704
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-14 10:31:22 -0500

    Little bit of docs

--- iter/seq.go
+++ iter/seq.go
@@ -5,6 +5,10 @@ import (
 )
 
 type (
+	// Seq is an iterator over sequences of individual values. When called as seq(yield),
+	// seq calls yield(v) for each value v in the sequence, stopping early if yield
+	// returns false. See the iter package documentation for more details.
+	// Will be replaced with a type alias when "generic type aliases" is a stable feature
 	Seq[V any] iter.Seq[V]
 )
 
