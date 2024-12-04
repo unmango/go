0f10674eacccf10530168d8d9b0485358204b557
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-04 09:35:23 -0600

    Copy fs util

---
+++ fs/copy.go
@@ -0,0 +1,29 @@
+package aferox
+
+import (
+	"io/fs"
+
+	"github.com/spf13/afero"
+)
+
+func Copy(src, dest afero.Fs) error {
+	return afero.Walk(src, "",
+		func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if path == "" {
+				return nil // Skip root
+			}
+			if info.IsDir() {
+				return dest.Mkdir(path, info.Mode())
+			}
+
+			if f, err := src.Open(path); err != nil {
+				return err
+			} else {
+				return afero.WriteReader(dest, path, f)
+			}
+		},
+	)
+}
