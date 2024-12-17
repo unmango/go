4708368adfb1c6e76661dde1e9400f6d2680021b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-16 20:06:32 -0600

    Refactored variable naming in AccessorFs
    The variable 'fs' in the AccessorFs struct has been renamed to 'Fs'. This change is reflected across all methods within the struct where this variable was used. The purpose of this refactoring is to improve code readability and maintain consistency with Go's naming conventions.

--- fs/context/accessor.go
+++ fs/context/accessor.go
@@ -23,72 +23,72 @@ func ToAccessor[T ~func() Context](fn T) aferox.ContextAccessor {
 // to source the [context.Context] for each operation
 type AccessorFs struct {
 	aferox.ContextAccessor
-	fs Fs
+	Fs Fs
 }
 
 // Chmod implements afero.Fs.
 func (a *AccessorFs) Chmod(name string, mode fs.FileMode) error {
-	return a.fs.Chmod(a.Context(), name, mode)
+	return a.Fs.Chmod(a.Context(), name, mode)
 }
 
 // Chown implements afero.Fs.
 func (a *AccessorFs) Chown(name string, uid int, gid int) error {
-	return a.fs.Chown(a.Context(), name, uid, gid)
+	return a.Fs.Chown(a.Context(), name, uid, gid)
 }
 
 // Chtimes implements afero.Fs.
 func (a *AccessorFs) Chtimes(name string, atime time.Time, mtime time.Time) error {
-	return a.fs.Chtimes(a.Context(), name, atime, mtime)
+	return a.Fs.Chtimes(a.Context(), name, atime, mtime)
 }
 
 // Create implements afero.Fs.
 func (a *AccessorFs) Create(name string) (afero.File, error) {
-	return a.fs.Create(a.Context(), name)
+	return a.Fs.Create(a.Context(), name)
 }
 
 // Mkdir implements afero.Fs.
 func (a *AccessorFs) Mkdir(name string, perm fs.FileMode) error {
-	return a.fs.Mkdir(a.Context(), name, perm)
+	return a.Fs.Mkdir(a.Context(), name, perm)
 }
 
 // MkdirAll implements afero.Fs.
 func (a *AccessorFs) MkdirAll(path string, perm fs.FileMode) error {
-	return a.fs.MkdirAll(a.Context(), path, perm)
+	return a.Fs.MkdirAll(a.Context(), path, perm)
 }
 
 // Name implements afero.Fs.
 func (a *AccessorFs) Name() string {
-	return fmt.Sprintf("Context: %s", a.fs.Name())
+	return fmt.Sprintf("Context: %s", a.Fs.Name())
 }
 
 // Open implements afero.Fs.
 func (a *AccessorFs) Open(name string) (afero.File, error) {
-	return a.fs.Open(a.Context(), name)
+	return a.Fs.Open(a.Context(), name)
 }
 
 // OpenFile implements afero.Fs.
 func (a *AccessorFs) OpenFile(name string, flag int, perm fs.FileMode) (afero.File, error) {
-	return a.fs.OpenFile(a.Context(), name, flag, perm)
+	return a.Fs.OpenFile(a.Context(), name, flag, perm)
 }
 
 // Remove implements afero.Fs.
 func (a *AccessorFs) Remove(name string) error {
-	return a.fs.Remove(a.Context(), name)
+	return a.Fs.Remove(a.Context(), name)
 }
 
 // RemoveAll implements afero.Fs.
 func (a *AccessorFs) RemoveAll(path string) error {
-	return a.fs.RemoveAll(a.Context(), path)
+	return a.Fs.RemoveAll(a.Context(), path)
 }
 
 // Rename implements afero.Fs.
 func (a *AccessorFs) Rename(oldname string, newname string) error {
-	return a.fs.Rename(a.Context(), oldname, newname)
+	return a.Fs.Rename(a.Context(), oldname, newname)
 }
 
 // Stat implements afero.Fs.
 func (a *AccessorFs) Stat(name string) (fs.FileInfo, error) {
-	return a.fs.Stat(a.Context(), name)
+	return a.Fs.Stat(a.Context(), name)
 }
 
 var _ afero.Fs = (*AccessorFs)(nil)
