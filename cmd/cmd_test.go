c153db307fa26ae7437fdd358401fc98c6ed2ae3
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-30 15:11:33 -0600

    Fail

---
+++ cmd/cmd_test.go
@@ -0,0 +1,25 @@
+package cmd_test
+
+import (
+	"os/exec"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Cmd", func() {
+	It("should fail how I want it to", func() {
+		p, err := gexec.Build("./testdata")
+		Expect(err).NotTo(HaveOccurred())
+		cmd := exec.Command(p, "The message")
+
+		ses, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(ses).Should(gexec.Exit(1))
+		Expect(ses.Err).To(gbytes.Say("The message\n"))
+		gexec.CleanupBuildArtifacts()
+	})
+})
