ad72355beada057781919968790e3a3236b668e6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-08 17:17:50 -0600

    A tad bit more extensibility to the git root function (#108)

--- vcs/git/root.go
+++ vcs/git/root.go
@@ -2,17 +2,28 @@ package git
 
 import (
 	"context"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 type (
-	wdKey struct{}
+	wdKey  struct{}
+	gitKey struct{}
 )
 
 func Root(ctx context.Context) (string, error) {
+	if env, ok := os.LookupEnv("GIT_ROOT"); ok {
+		return env, nil
+	}
+
+	git, err := gitPath(ctx)
+	if err != nil {
+		return "", err
+	}
+
 	cmd := exec.CommandContext(ctx,
-		"git", "rev-parse", "--show-toplevel",
+		git, "rev-parse", "--show-toplevel",
 	)
 	if wd, ok := ctx.Value(wdKey{}).(string); ok && wd != "" {
 		cmd.Dir = wd
@@ -26,6 +37,22 @@ func Root(ctx context.Context) (string, error) {
 	return strings.TrimSpace(string(output)), nil
 }
 
+func WithGitPath(parent context.Context, path string) context.Context {
+	return context.WithValue(parent, gitKey{}, path)
+}
+
 func WithWorkingDirectory(parent context.Context, path string) context.Context {
 	return context.WithValue(parent, wdKey{}, path)
+}
+
+func gitPath(ctx context.Context) (string, error) {
+	if git := ctx.Value(gitKey{}); git != nil {
+		return git.(string), nil
+	}
+
+	if git := os.Getenv("GIT_PATH"); git != "" {
+		return git, nil
+	}
+
+	return exec.LookPath("git")
 }
--- vcs/git/root_test.go
+++ vcs/git/root_test.go
@@ -3,6 +3,7 @@ package git_test
 import (
 	"context"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -38,5 +39,48 @@ var _ = Describe("Root", func() {
 		Expect(err).NotTo(HaveOccurred())
 		p = strings.TrimPrefix(p, "/private") // Mac crap
 		Expect(p).To(Equal(wd))
+	})
+
+	It("should print the git root from the environment", func(ctx context.Context) {
+		os.Setenv("GIT_ROOT", "/some/random/path")
+		DeferCleanup(func() {
+			os.Unsetenv("GIT_ROOT")
+		})
+
+		p, err := git.Root(ctx)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal("/some/random/path"))
+	})
+
+	It("should use git from the environment", func(ctx context.Context) {
+		wd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		expected := strings.TrimSuffix(wd, "/vcs/git")
+		exe, err := exec.LookPath("git")
+		Expect(err).NotTo(HaveOccurred())
+
+		os.Setenv("GIT_PATH", exe)
+		DeferCleanup(func() {
+			os.Unsetenv("GIT_PATH")
+		})
+
+		p, err := git.Root(ctx)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal(expected))
+	})
+
+	It("should use git from the context", func(ctx context.Context) {
+		wd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		expected := strings.TrimSuffix(wd, "/vcs/git")
+		exe, err := exec.LookPath("git")
+		Expect(err).NotTo(HaveOccurred())
+
+		p, err := git.Root(git.WithGitPath(ctx, exe))
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal(expected))
 	})
 })
