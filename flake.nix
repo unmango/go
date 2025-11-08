9a73512c054b0039468a936ccacf1f427a15ea11
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-08 17:48:33 -0600

    fmt

--- flake.nix
+++ flake.nix
@@ -3,31 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-		treefmt-nix = {
-			url = "github:numtide/treefmt-nix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix }: flake-utils.lib.eachDefaultSystem (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in
-		{
-			formatter = treefmt-nix.lib.mkWrapper pkgs {
-				projectRootFile = "flake.nix";
-				programs.gofmt.enable = true;
-			};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs.gofmt.enable = true;
+          programs.nixfmt.enable = true;
+        };
 
-			devShells.default = pkgs.mkShell {
-				buildInputs = with pkgs; [
-					git
-					gnumake
-					go
-					ginkgo
-				];
-			};
-		}
-	);
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            gnumake
+            go
+            ginkgo
+          ];
+        };
+      }
+    );
 }
