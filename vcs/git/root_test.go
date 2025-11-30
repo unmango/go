f72c7281bf07df54a9e7bc7d157bfeb7ca25c8d3
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-30 11:08:48 -0600

    Refactoring the git root signature (#121)

--- vcs/git/root_test.go
+++ vcs/git/root_test.go
@@ -14,6 +14,77 @@ import (
 	"github.com/unmango/go/vcs/git"
 )
 
+var _ = Describe("RootContext", Label("Dependency:Git"), func() {
+	It("should print the current git root", func(ctx context.Context) {
+		wd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		expected := strings.TrimSuffix(wd, "/vcs/git")
+
+		p, err := git.RootContext(ctx)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal(expected))
+	})
+
+	It("should print the working directory's git root", func(ctx context.Context) {
+		wd := GinkgoT().TempDir()
+		testing.GitInit(ctx, wd)
+
+		By("Creating a subdirectory")
+		subdir := filepath.Join(wd, "subdir")
+		Expect(os.Mkdir(subdir, os.ModePerm)).To(Succeed())
+
+		p, err := git.RootContext(git.WithWorkingDirectory(ctx, subdir))
+
+		Expect(err).NotTo(HaveOccurred())
+		p = strings.TrimPrefix(p, "/private") // Mac crap
+		Expect(p).To(Equal(wd))
+	})
+
+	It("should print the git root from the environment", func(ctx context.Context) {
+		os.Setenv("GIT_ROOT", "/some/random/path")
+		DeferCleanup(func() {
+			os.Unsetenv("GIT_ROOT")
+		})
+
+		p, err := git.RootContext(ctx)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal("/some/random/path"))
+	})
+
+	It("should use git from the environment", func(ctx context.Context) {
+		wd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		expected := strings.TrimSuffix(wd, "/vcs/git")
+		exe, err := exec.LookPath("git")
+		Expect(err).NotTo(HaveOccurred())
+
+		os.Setenv("GIT_PATH", exe)
+		DeferCleanup(func() {
+			os.Unsetenv("GIT_PATH")
+		})
+
+		p, err := git.RootContext(ctx)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal(expected))
+	})
+
+	It("should use git from the context", func(ctx context.Context) {
+		wd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		expected := strings.TrimSuffix(wd, "/vcs/git")
+		exe, err := exec.LookPath("git")
+		Expect(err).NotTo(HaveOccurred())
+
+		p, err := git.RootContext(git.WithGitPath(ctx, exe))
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(p).To(Equal(expected))
+	})
+})
+
 var _ = Describe("Root", Label("Dependency:Git"), func() {
 	It("should print the current git root", func(ctx context.Context) {
 		wd, err := os.Getwd()
