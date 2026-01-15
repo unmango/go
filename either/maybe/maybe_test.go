84bc7a5428186428dbe7208d58759515412afce4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-01-15 11:54:11 -0600

    Copy over the copilot changes

--- either/maybe/maybe_test.go
+++ either/maybe/maybe_test.go
@@ -27,4 +27,63 @@ var _ = Describe("Maybe", func() {
 			Expect(v).To(BeNil())
 		})
 	})
+
+	Describe("Map", func() {
+		It("should map Some value", func() {
+			m := maybe.Some(42)
+
+			mapped := maybe.Map(m, func(v int) int {
+				return v * 2
+			})
+
+			v := mapped()
+			Expect(*v).To(Equal(84))
+		})
+
+		It("should not map None value", func() {
+			m := maybe.None[int]()
+
+			mapped := maybe.Map(m, func(v int) int {
+				return v * 2
+			})
+
+			v := mapped()
+			Expect(v).To(BeNil())
+		})
+	})
+
+	Describe("Bind", func() {
+		It("should bind Some value", func() {
+			m := maybe.Some(42)
+
+			bound := maybe.Bind(m, func(v int) maybe.Maybe[int] {
+				return maybe.Some(v * 2)
+			})
+
+			v := bound()
+			Expect(*v).To(Equal(84))
+		})
+
+		It("should not bind None value", func() {
+			m := maybe.None[int]()
+
+			bound := maybe.Bind(m, func(v int) maybe.Maybe[int] {
+				return maybe.Some(v * 2)
+			})
+
+			v := bound()
+			Expect(v).To(BeNil())
+		})
+
+		It("should propagate None from bind function", func() {
+			m := maybe.Some(42)
+
+			bound := maybe.Bind(m, func(v int) maybe.Maybe[int] {
+				return maybe.None[int]()
+			})
+
+			v := bound()
+			Expect(v).To(BeNil())
+		})
+	})
 })
